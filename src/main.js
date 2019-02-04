0fe5ac73cb244a3723665b3314d8df66a1451aa2
Art Dahm <art@dahm.com>
2019-02-04 11:20:29 -0500

    Initial authorization code

--- src/main.js
+++ src/main.js
@@ -2,11 +2,34 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import '@/firebase/init'
+import firebase from 'firebase'
 
 Vue.config.productionTip = false
 
-new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount('#app')
+let app = null
+// const config = {
+//   apiKey: process.env.VUE_APP_FIREBASE_API_KEY,
+//   authDomain: process.env.VUE_APP_FIREBASE_AUTH_DOMAIN,
+//   databaseURL: process.env.VUE_APP_FIREBASE_DATABASE_URL,
+//   projectId: process.env.VUE_APP_FIREBASE_PROJECT_ID,
+//   storageBucket: process.env.VUE_APP_FIREBASE_STORAGE_BUCKET,
+//   messagingSenderId: process.env.VUE_APP_FIREBASE_MESSAGING_SENDER_ID
+// }
+//
+// firebase.initializeApp(config)
+
+// wait for firebase auth to init before creating the app
+firebase.auth().onAuthStateChanged(() => {
+
+  // init app if not already created
+  if(!app){
+    app = new Vue({
+      router,
+      store,
+      render: h => h(App)
+    }).$mount('#app')
+  }
+
+})
+
--- src/router.js
+++ src/router.js
@@ -1,10 +1,13 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import firebase from 'firebase'
 import Home from './views/Home.vue'
+import Signup from './views/Signup.vue'
+import Login from './views/Login.vue'
 
 Vue.use(Router)
 
-export default new Router({
+const router = new Router({
   routes: [
     {
       path: '/',
@@ -12,12 +15,31 @@ export default new Router({
       component: Home
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+      path: '/signup',
+      name: 'Signup',
+      component: Signup
+    },
+    {
+      path: '/login',
+      name: 'Login',
+      component: Login
     }
   ]
 })
+
+router.beforeEach((to, from, next) => {
+  const currentUser = firebase.auth().currentUser;
+  const requiresAuth = to.matched.some(record => record.meta.requiresAuth)
+
+  if (requiresAuth && !currentUser) {
+    next('login')
+  }
+  else if (!requiresAuth && currentUser) {
+    next('home')
+  }
+  else {
+    next()
+  }
+})
+
+export default router
